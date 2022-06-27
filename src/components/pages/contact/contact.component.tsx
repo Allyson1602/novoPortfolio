c588ad0175f5a01041ec40316a1c06bfb8d1ddc7
Allyson1602 <aathyrson@yahoo.com.br>
2022-06-27 20:56:59 -0300

    Criação de Component controlled para o Contact

--- src/components/pages/contact/contact.component.tsx
+++ src/components/pages/contact/contact.component.tsx
@@ -1,18 +1,178 @@
-import { FC } from "react";
+/* eslint-disable no-useless-escape */
+import { User, At, Envelope, Check } from "phosphor-react";
+import { FC, useState, FormEvent } from "react";
 
 import "./contact.style.css";
 
+interface IFormData {
+    name: string;
+    email: string;
+    message: string;
+}
+
+interface IValidationData {
+    status: boolean;
+    text?: string;
+}
+
 const Contact: FC = () => {
+    const [name, setName] = useState<string | null>(null);
+    const [email, setEmail] = useState<string | null>(null);
+    const [message, setMessage] = useState<string | null>(null);
+
+    const ValidationDatas = (): IValidationData => {
+        let pattern;
+
+        if(!name || !email || !message) {
+            return {
+                status: false,
+                text: "Preencha todos os campos"
+            }
+        }
+
+        pattern = /([^A-Za-z\s])/g;
+        let validationName = !pattern.test(name);
+        if(!validationName) {
+            return {
+                status: false,
+                text: "Informe o nome corretamente"
+            }
+        }
+
+        pattern = /([^A-Za-z0-9\_\-\@\.\n])/g;
+        let validationEmail = !pattern.test(email);
+        if(!validationEmail) {
+            return {
+                status: false,
+                text: "Informe o email corretamente"
+            }
+        }
+
+        return {
+            status: true,
+        };
+    }
+
+    const getDatas = (): IFormData | null => {
+        if(!name || !email || !message) return null;
+
+        return {
+            name: name,
+            email: email,
+            message: message
+        };
+    };
+
+    const handleSubmit = (ev: FormEvent) => {
+        ev.preventDefault();
+
+        if(ValidationDatas().status) {
+            console.log(getDatas());
+        }
+    };
+
     return (
-        <div className="container-fluid">
-            <div className="row p-60-0 p-lg-30-0 p-md-15-0">
-                <div className="col-lg-12">
-                    <div className="">
-                        <h1>Contato</h1>
+        <>
+            <div className="container-fluid">
+                <div className="row p-30-0">
+                    <div className="col-lg-12">
+                        <div className="art-section-title">
+                            <div className="art-title-frame">
+                                <h4>Informações de contato</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="data-contact-information">
+                        <div className="col-12 col-xl-4">
+                            <div className="art-a art-card">
+                                <div className="art-table p-15-15">
+                                    <ul>
+                                        <li>
+                                            <h6>Email:</h6><span>athyrsonallyson@gmail.com</span>
+                                        </li>
+                                        <li>
+                                            <h6>Discord:</h6><span>Allyson Athyrson#7485</span>
+                                        </li>
+                                        <li>
+                                            <h6>Workana:</h6><span>Allyson Athyrson</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-xl-4">
+                            <div className="art-a art-card">
+                                <div className="art-table p-15-15">
+                                    <ul>
+                                        <li>
+                                            <h6>Instagram:</h6><span>@allysonathyrson</span>
+                                        </li>
+                                        <li>
+                                            <h6>Telefone:</h6><span>(61) 9 9396-8144</span>
+                                        </li>
+                                        <li>
+                                            <h6>Facebook:</h6><span>Allyson Athyrson</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-12">
+                        <div className="art-section-title">
+                            <div className="art-title-frame">
+                                <h4>Entrar em contato</h4>
+                            </div>
+                        </div>
+                        <div className="art-a art-card">
+                            <form id="form" className="art-contact-form" onSubmit={handleSubmit}>
+                                <div className="art-form-field">
+                                    <input
+                                        id="name"
+                                        className="art-input"
+                                        type="text"
+                                        placeholder="Nome"
+                                        value={name || ""}
+                                        onChange={(ev) => setName(ev.target.value)}
+                                    />
+                                    <label htmlFor="name"><User size={20} weight="fill" /></label>
+                                </div>
+                                <div className="art-form-field">
+                                    <input
+                                        id="email"
+                                        className="art-input"
+                                        type="email"
+                                        placeholder="Email"
+                                        value={email || ""}
+                                        onChange={(ev) => setEmail(ev.target.value)}
+                                    />
+                                    <label htmlFor="email"><At size={20} weight="fill" /></label>
+                                </div>
+                                <div className="art-form-field">
+                                    <textarea
+                                        id="message"
+                                        className="art-input"
+                                        placeholder="Mensagem"
+                                        value={message || ""}
+                                        onChange={(ev) => setMessage(ev.target.value)}
+                                    ></textarea>
+                                    <label htmlFor="message"><Envelope size={20} weight="fill" /></label>
+                                </div>
+                                <div className="art-submit-frame">
+                                    <button className="art-btn art-btn-md art-submit" type="submit"><span>Enviar mensagem</span></button>
+                                    {
+                                        ValidationDatas().status
+                                        ? <div className="art-success">Success <Check size={20} weight="fill" /></div>
+                                        : <div className="art-success">{ValidationDatas().text}</div>
+                                    }
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/components/pages/portfolio/portfolio.component.tsx
+++ src/components/pages/portfolio/portfolio.component.tsx
@@ -4,15 +4,168 @@ import "./portfolio.style.css";
 
 const Portfolio: FC = () => {
     return (
-        <div className="container-fluid">
-            <div className="row p-60-0 p-lg-30-0 p-md-15-0">
-                <div className="col-lg-12">
-                    <div className="">
-                        <h1>Portfolio</h1>
+        <>
+            <div className="container-fluid">
+                <div className="row p-30-0">
+                    <div className="col-lg-12">
+                        <div className="art-filter mb-30">
+                            <a href="#" data-filter=".webTemplates" className="art-link">Páginas Web</a>
+                        </div>
+                    </div>
+
+                    <div className="art-grid art-grid-3-col art-gallery">
+
+                        <div className="art-grid-item webTemplates">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/1.jpg" className="art-a art-portfolio-item-frame art-horizontal">
+                                <img src="img/works/thumbnail/1.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item logos">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/2.jpg" className="art-a art-portfolio-item-frame art-vertical">
+                                <img src="img/works/thumbnail/2.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <div className="mb-15">Sit amet, consectetur adipisicing elit. Quas, architecto.</div>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item drawings">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/3.jpg" className="art-a art-portfolio-item-frame art-square">
+                                <img src="img/works/thumbnail/3.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <div className="mb-15">Sit amet, consectetur adipisicing elit. Quas, architecto.</div>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item ui">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/4.jpg" className="art-a art-portfolio-item-frame art-vertical">
+                                <img src="img/works/thumbnail/4.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <div className="mb-15">Sit amet, consectetur adipisicing elit. Quas, architecto.</div>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item webTemplates">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/5.jpg" className="art-a art-portfolio-item-frame art-square">
+                                <img src="img/works/thumbnail/5.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <div className="mb-15">Sit amet, consectetur adipisicing elit. Quas, architecto.</div>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item logos">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/6.jpg" className="art-a art-portfolio-item-frame art-vertical">
+                                <img src="img/works/thumbnail/6.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <div className="mb-15">Sit amet, consectetur adipisicing elit. Quas, architecto.</div>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item drawings">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/7.jpg" className="art-a art-portfolio-item-frame art-square">
+                                <img src="img/works/thumbnail/7.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <div className="mb-15">Sit amet, consectetur adipisicing elit. Quas, architecto.</div>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item ui">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/8.jpg" className="art-a art-portfolio-item-frame art-horizontal">
+                                <img src="img/works/thumbnail/8.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item webTemplates">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/9.jpg" className="art-a art-portfolio-item-frame art-horizontal">
+                                <img src="img/works/thumbnail/9.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item logos">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/10.jpg" className="art-a art-portfolio-item-frame art-horizontal">
+                                <img src="img/works/thumbnail/10.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
+
+                        <div className="art-grid-item drawings">
+                            <a data-fancybox="gallery" data-no-swup href="img/works/original-size/11.jpg" className="art-a art-portfolio-item-frame art-horizontal">
+                                <img src="img/works/thumbnail/11.jpg" alt="item" />
+                                <span className="art-item-hover"><i className="fas fa-expand"></i></span>
+                            </a>
+                            <div className="art-item-description">
+                                <h5 className="mb-15">Project title</h5>
+                                <a href="portfolio-single.html" className="art-link art-color-link art-w-chevron">Read more</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className="container-fluid">
+                <div className="row">
+
+                    <div className="col-6 col-lg-3">
+                        <img className="art-brand" src="img/brands/1.png" alt="brand" />
+                    </div>
+
+                    <div className="col-6 col-lg-3">
+                        <img className="art-brand" src="img/brands/2.png" alt="brand" />
+                    </div>
+
+                    <div className="col-6 col-lg-3">
+                        <img className="art-brand" src="img/brands/3.png" alt="brand" />
+                    </div>
+
+                    <div className="col-6 col-lg-3">
+                        <img className="art-brand" src="img/brands/1.png" alt="brand" />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
